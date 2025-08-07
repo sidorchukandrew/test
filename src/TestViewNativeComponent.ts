97dd620d98271c8dd7839c6c6c8e81ddf11f763e
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2025-08-06 22:30:42 -0400

    chore: make it interface only

--- src/TestViewNativeComponent.ts
+++ src/TestViewNativeComponent.ts
@@ -5,4 +5,6 @@ interface NativeProps extends ViewProps {
   color?: string;
 }
 
-export default codegenNativeComponent<NativeProps>('TestView');
+export default codegenNativeComponent<NativeProps>('TestView', {
+  interfaceOnly: true,
+});
